5160786a7eb3eeb4d1031680171f6b4d0fe408e4
Maros Tomasov <maros3845@gmail.com>
2022-11-28 18:42:13 +0100

    Updates

--- TicTacToe/mouse.py
+++ TicTacToe/mouse.py
@@ -6,5 +6,6 @@ class Mouse():
     
     def triggered(self,pos):
         self.mx,self.my = pos
-        print(self.mx)
+        if self.mx >= 0 and self.mx <= 300 and self.my >= 0 and self.my <= 300:
+            print("it is in the first box")
         
