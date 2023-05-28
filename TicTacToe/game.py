4bddde6db2a525d5fee28aab7d657e8b18c36243
Maros Tomasov <maros3845@gmail.com>
2023-05-28 15:43:41 +0200

    Last Update

--- TicTacToe/game.py
+++ TicTacToe/game.py
@@ -2,6 +2,7 @@
 import pygame,json
 from TicTacToe.mouse import Mouse
 from TicTacToe.render import Rendering
+import random
 
 
  #* Main game class containing main loop for the game
@@ -18,20 +19,28 @@ class Game:
         self.quit = False
         self.win = False
         self.inmenu = True
+        self.start = True 
         self.win_state = [[1,2,3],[4,5,6],[7,8,9], [1,4,7], [2,5,8],[3,6,9],[1,5,9],[3,5,7]]
-        self.mouse = Mouse(self.screen)
+        self.mouse = Mouse(self.screen, self)
         #* This indicates whether the square is empty or there is X or O
         #* E = Empty | 
         self.grid_index = ['E','E','E','E','E','E','E','E','E']
         #* Who's turn it is | 0 = X| 1 = O| 
         self.turn = 0
+        self.but_ind = 2
         self.pos_i = None
+        self.player_state = 0
         self.render = Rendering(self.data["WIDTH"],self.data["HEIGHT"], self.screen)
         
     
     #* Resets data to default for a new game, but the game score is saved
     def restart(self):
         self.grid_index = ['E','E','E','E','E','E','E','E','E']
+        self.win = False
+
+    def random_field(self):
+        random = random.randint(0,8)
+
 
     #* Iterates through loop and checks and checks for the same positions
     def check_win(self):
@@ -44,8 +53,17 @@ class Game:
 
     #* Event functions that checks for keyboard presses and manipulation with the game
     def events(self):
+        
+        if not self.win and not self.inmenu and self.player_state == 1 and self.turn%2 == 1:
+            randomint = random.randint(0,8)
+            if self.grid_index[randomint] == 'E':
+                    self.grid_index[randomint] = 'O'
+                    self.turn += 1
+        else:
+            pass
+        print(self.player_state)
         for event in pygame.event.get():
-
+            
             #* Checks if the close button is pressed
             if event.type == pygame.QUIT:
                 self.quit = True
@@ -60,8 +78,21 @@ class Game:
                     self.quit = True
 
             #* If the mouse button is pressed (any button) the function will find which square it is located 
-            if event.type == pygame.MOUSEBUTTONDOWN and not self.win and not self.inmenu:
+            if event.type == pygame.MOUSEBUTTONDOWN and not self.win and not self.inmenu and self.player_state == 1:
                 box = self.mouse.triggered()
+                
+
+                if self.turn%2 == 0 and self.grid_index[box] == 'E':
+                    self.grid_index[box] = 'X'
+                    self.turn += 1
+
+                else:
+                    pass
+            
+            if event.type == pygame.MOUSEBUTTONDOWN and not self.win and not self.inmenu and self.player_state == 2:
+                box = self.mouse.triggered()
+                
+
                 if self.turn%2 == 0 and self.grid_index[box] == 'E':
                     self.grid_index[box] = 'X'
                     self.turn += 1
@@ -70,18 +101,68 @@ class Game:
                     self.turn += 1
                 else:
                     pass
+                
+            
                 self.check_win()
+            if event.type == pygame.MOUSEBUTTONDOWN and self.start and self.inmenu:
+                if self.but_ind == 0:
+                    self.start = False
+                    self.inmenu = False
+                    self.player_state = 1
+                    self.one_player_loop()
+
+                if self.but_ind == 1:
+                    self.start = False
+                    self.inmenu = False
+                    self.player_state = 2
+                    self.two_player_loop()
                 
     def start_loop(self):
+        text_1_col = (255,255,255)
+        text_2_col = (255,255,255)
+
+        
         while not self.quit:
             self.events()
-            self.mouse.hover_on(self.data["WIDTH"], self.data["HEIGHT"])
-            self.render.render_text((self.data["WIDTH"]/4-64), (self.data["HEIGHT"]/2), 'ONE PLAYER', (255,255,255),32)
-            self.render.render_text((self.data["WIDTH"]/2+64),self.data["HEIGHT"]/2, 'TWO PLAYERS',(255,255,255),32)
+
+            if self.mouse.hover_on(self.data["WIDTH"] / 4 - 64, self.data["HEIGHT"] / 2, 200, 25):
+                self.but_ind = 0
+                text_1_col = (200,200,200)
+
+            elif self.mouse.hover_on(self.data["WIDTH"]/ 2 + 64, self.data["HEIGHT"] / 2, 240, 25):
+                self.but_ind = 1
+                text_2_col = (200,200,200)
+
+            else:
+                self.but_ind = 2
+                text_2_col = (255,255,255)
+                text_1_col = (255,255,255) 
+
+            #* Button one for one player but_ind - 0
+            self.render.render_text((self.data["WIDTH"]/4-64), (self.data["HEIGHT"]/2), 'ONE PLAYER', text_1_col,32)
+                
+            #* Button two for two player but_ind - 1
+            self.render.render_text((self.data["WIDTH"]/2+64),self.data["HEIGHT"]/2, 'TWO PLAYERS', text_2_col,32)
+
+
+
             self.render.render_text((self.data["WIDTH"]/4+10  ),self.data["HEIGHT"]/6,'TIC TAC TOE', (255,255,255),64)
             pygame.display.flip()
 
-    def Loop(self):
+    def two_player_loop(self):
+        while not self.quit:
+            self.screen.fill((0,0,0))
+            if self.win:
+                self.render.render_win(self.win_state,self.pos_i)
+
+            self.events()
+            #* Draws a grid to show places to put X or O
+            self.render.drawGrid()
+            #* Renders players X or O
+            self.render.render_grid(self.grid_index)
+            pygame.display.update()
+    
+    def one_player_loop(self):
         while not self.quit:
             self.screen.fill((0,0,0))
             if self.win:
--- TicTacToe/mouse.py
+++ TicTacToe/mouse.py
@@ -1,19 +1,26 @@
 import pygame
 
 class Mouse():
-    def __init__(self, screen) -> None:
+    def __init__(self, screen, game) -> None:
         super().__init__()
         self.screen = screen
+        self.game = game
     
     def triggered(self):
-        mx,my = pygame.mouse.get_pos()
+        self.mx,self.my = pygame.mouse.get_pos()
         trigers_on = [[0,0,300,300],[300,0,600,300],[600,0,900,300],[0,300,300,600],[300,300,600,600],[600,300,900,600],[0,600,300,900],[300,600,600,900],[600,600,900,900]]
         for i,item in enumerate(trigers_on):
-            if mx > item[0] and mx < item[2] and my > item[1] and my < item[3]:
+            if self.mx > item[0] and self.mx < item[2] and self.my > item[1] and self.my < item[3]:
                 return i
-    def Clicks_on(self, width,height):
-        mx,my = pygame.mouse.get_pos()
-        pygame.draw.line(self.screen,(255,255,255,),((width/2+64),(height/2-30),), ((width/2+64+32*7.2),(height/2-30)), 4)
-        
 
-        
+    def hover_on(self, x,y, width, height):
+        # pygame.draw.line(self.screen,(255,0,0,),(x,y), (x+width, y+height), 4)
+        mx,my = pygame.mouse.get_pos()
+
+        in_x = mx >= x and mx <= x+width
+        in_y = my >= y and my <= y+height
+
+        if in_x and in_y:
+            return True
+        else:
+            return False
